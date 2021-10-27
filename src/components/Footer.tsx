99c7b932907db284022f2ea0d13f016e8bb99c8e
jeremy <jeremy>
2021-10-27 15:52:51 +1100

    Add Splashup icon in the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
 
+import splashupIcon from '../_images/icon-splashup.png'
+
 const Footer = (): JSX.Element => (
-  <footer className="my-4 text-center">
-    <p className="text-sm ">
+  <footer className="flex items-center justify-center my-4 text-center ">
+    <img src={splashupIcon} className="h-4 mr-1" alt="splashup" />
+    <p className="text-sm">
       powered by{' '}
       <a href="https://splashup.co" target="_blank" rel="noopener noreferrer" className="text-secondary">
         splashup.co
