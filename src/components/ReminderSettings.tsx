e257787d11d98479d8db7b94462ba81dd0d6fa00
pshepelenko <shepelenko.p@gmail.com>
2022-05-16 18:53:23 +0300

    reminder popup update

--- src/components/ReminderSettings.tsx
+++ src/components/ReminderSettings.tsx
@@ -10,7 +10,7 @@ const ReminderSettings: FC<IProps> = ({ type = 'e-mail', closePopup, saveSetting
   return (
     <div className='flex items-center justify-center'>
       <div onClick={() => {closePopup(true)}} className="fixed top-0 left-0 bg-gray-500/50 w-full h-full z-[102] flex items-center justify-center" />
-      <div className="fixed top-1/4 bg-gray-100 w-80 h-80 z-[102] px-10 py-10" >
+      <div className="fixed top-1/4 bg-gray-100 w-80 z-[102] px-10 py-10" >
         <div className='flex flex-row bg-secondary text-white h-10 w-full absolute top-0 left-0 px-2 justify-between items-center'> 
           <div className='w-5'/>
           REMIND ME 
@@ -59,7 +59,7 @@ const ReminderSettings: FC<IProps> = ({ type = 'e-mail', closePopup, saveSetting
           </div>
         </div>
 
-        <div className='flex flex-row absolute left-0 bottom-3 w-full justify-center'>
+        <div className='flex flex-row mt-5 w-full justify-center'>
           <button className='py-1 px-2 w-40 text-sm font-medium border shadow-sm focus:none mb-3 flex items-center justify-center text-secondary bg-secondary text-white' onClick={() => {saveSettings(quantity, scale)}}> Submit </button>
             
         </div>
